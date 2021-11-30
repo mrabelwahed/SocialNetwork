97e64ec5d2dc758fa415187723ec214cf7365c6a
Mahmoud Ramadan <eng.mahmoudramadan2012@gmail.com>
2021-11-30 16:29:33 +0200

    work in progress

--- register.php
+++ register.php
@@ -5,6 +5,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+//db connection
+$con = mysqli_connect("localhost","root","","social");
+if(mysqli_connect_errno()){
+	echo "failed to connect".mysqli_connect_errno();
+}
 
 //variables 
 $fname = "";
@@ -51,16 +56,46 @@ $date = date("Y-m-d");
 //check if emails matched
 
 if ($em == $em2) {
-	if(filter_var($em, FILTER_VALIDATE_EMAIL))
+	if(filter_var($em, FILTER_VALIDATE_EMAIL)){
+
 		$em = filter_var($em, FILTER_VALIDATE_EMAIL);
+		// check if email is used
+		$e_check = mysqli_query($con,"SELECT email FROM users WHERE email='$em'");
+		$num_rows = mysqli_num_rows($e_check);
+		if ($num_rows > 0) {
+		   echo "Email already in use<br>";
+		}
+	}
 	else
-		echo "Invalid formats";
+		echo "Invalid formats<br>";
 }else{
-	echo "Emails do not match";
+	echo "Emails do not match<br>";
 }
 
+// validate first name
 
+if (strlen($fname) >25 || strlen($fname) <2) {
+	echo "Your first name must between 2 and 25 <br>";
+}
 
+// validate last name
+
+if (strlen($lname) >25 || strlen($lname) <2) {
+	echo "Your last name must between 2 and 25<br>";
+}
+
+// validate passwords
+if ($password != $password2) {
+	echo "Your passeord do not match<br>";
+}else{
+	if (preg_match('/[^A-Za-z0-9]/', $password)) {
+		echo "your password can contain only english alphabets abd numbers <br>";
+	}
+}
+
+if (strlen($password) > 30 || strlen($password2) < 5) {
+	echo "Your password must between 5 and 30 characters <br>";
+}
 
 
 }
